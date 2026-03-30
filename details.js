edc9e17ff81048428255b677769db3ca3b66f0d1
dyasse <32208196+dyasse@users.noreply.github.com>
2026-03-30 17:22:50 +0100

    Update details.js

--- details.js
+++ details.js
@@ -1,3 +1,10 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker خدام بنجاح!'))
+      .catch(err => console.log('وقع مشكل في الـ Service Worker', err));
+  });
+}
 async function loadDetails() {
   const params = new URLSearchParams(window.location.search);
   const chapterId = parseInt(params.get("id"));
