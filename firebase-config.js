3bf1f9468ac2886a5451410b5e59172dcb1d7fac
dyasse <32208196+dyasse@users.noreply.github.com>
2026-04-02 12:51:08 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,11 @@
+// firebase-config.js
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyAVixG188LWr0s-y3bhQsBerXK4YK-Al2E",
+  authDomain: "nour-3f6d4.firebaseapp.com",
+  projectId: "nour-3f6d4",
+  storageBucket: "nour-3f6d4.firebasestorage.app",
+  messagingSenderId: "301905677274",
+  appId: "1:301905677274:web:1427326a07085430b0cee3",
+  measurementId: "G-G92Q1BWMBT"
+};
