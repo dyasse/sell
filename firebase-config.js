1d3c8a586595fb1cbc1ccdb5a0c456a85727d252
dyasse <32208196+dyasse@users.noreply.github.com>
2026-04-06 22:23:23 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,14 +7,15 @@ const firebaseConfig = {
   projectId: "nour-3f6d4",
   storageBucket: "nour-3f6d4.firebasestorage.app",
   messagingSenderId: "301905677274",
-  appId: "1:301905677274:web:1427326a07085430b0cee3",
-  measurementId: "G-G92Q1BWMBT"
+  appId: "1:301905677274:web:1427326a07085430b0cee3"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export objects bach n-khdmo bihom f les pages khrin
 export const auth = getAuth(app);
+
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
+
 export const facebookProvider = new FacebookAuthProvider();
+facebookProvider.addScope("email");
